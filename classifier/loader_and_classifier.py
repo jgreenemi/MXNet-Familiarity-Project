4793f5169762614a772b3a77f5c2b597d33a4c70
Joseph Greene <jgreenemi@gmail.com>
2017-12-13 13:00:00 -0800

    Introduce string cleaner, data loader.

--- classifier/loader_and_classifier.py
+++ classifier/loader_and_classifier.py
@@ -6,3 +6,33 @@ import re
 from urllib.request import urlopen
 from collections import Counter
 
+datafile_url_prefix = 'https://raw.githubusercontent.com/jgreenemi/MXNet-Familiarity-Project/master/'
+datafile_positive = 'rt-positive.txt'
+datafile_negative = 'rt-negative.txt'
+
+def clean_strings(string):
+    """
+    Clean up strings for better processing.
+    :param string:
+    :return:
+    """
+
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+
+    # I'm not sure what's the reason for tokenizing strings' concatenations in this manner.
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    # More can/should be added.
+
+    # Trim erroneous spaces from strings, force to lowercase, and return.
+    return string.strip().lower()
+
+
+def load_data_with_labels():
+    """
+    Pull in data from files, return the split sentences and labels.
+    :return:
+    """
